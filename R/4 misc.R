c7cf73f6730b07a79fc0b9ec7b1cbf50ae5e0a31
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-02-18 12:43:49 +0000

    Update 4 misc.R

--- R/4 misc.R
+++ R/4 misc.R
@@ -1,7 +1,19 @@
+# distress by discipline, controlling for age, seniority
 
 
-# specific plots
+data_s1 <- read_rds(file=here::here("processed-data",  "data_s1.rds")) 
 
-# reliability
 
-# etc
+mod0_dept <- lm(formula = ghqsum0123_s1 ~ dept_ed + dept_anaes+ dept_icu, data = data_s1)
+mod1_dept <- lm(formula = ghqsum0123_s1 ~ dept_ed + dept_anaes+ dept_icu + age + seniority, data = data_s1)
+
+summary(mod0_dept)
+summary(mod1_dept)
+
+
+mod0_specialty <- lm(formula = ghqsum0123_s1 ~ comb_specialty2, data = data_s1)
+mod1_specialty <- lm(formula = ghqsum0123_s1 ~ comb_specialty2 + age + seniority, data = data_s1)
+
+summary(mod0_specialty)
+summary(mod1_specialty)
+
